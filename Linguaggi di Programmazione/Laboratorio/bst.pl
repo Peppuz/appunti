96d5e6212f134f47bd42170a1ab149806936ba48
bigboss98 <marco_natali@ymail.com>
2018-10-31 12:22:11 +0100

    Update bst.pl

--- Linguaggi di Programmazione/Laboratorio/bst.pl
+++ Linguaggi di Programmazione/Laboratorio/bst.pl
@@ -17,6 +17,17 @@ new_tree(Name) :-
     atomic(Name),
     assert(tree(Name, void)).
 
+delete_tree(Name) :-
+    retractall().
+
+delete_node() :-
+
+print_tree() :-
+
+min_tree() :-
+
+max_tree() :-
+
 tree_insert(K, V, Name, tree(Name, NewRoot)) :-
     tree(Name, Root),
     node_insert(K, V, Root, NewRoot),
@@ -52,6 +63,15 @@ node_search(K, node(NK, _NV, _L, R), V) :-
 node_search(K, node(NK, NV, _L, _R), V) :-
     compare('=', K, NK),
     V = NV.
+
+:- delete_tree(movies).
+:- new_tree(movies).
+:- tree_insert(1980, "The Blues Brothers", movies, _).
+:- tree_insert(2016, "Soldado", movies, _).
+:- tree_insert(1994, "Pulp Fiction", movies, _).
+:- tree_insert(2018, "Ready Player One", movies, _).
+:- tree_insert(2000, "Gladiator", movies, _).
+:- tree_insert(1996, "Memento", movies, _).
 
 
 
