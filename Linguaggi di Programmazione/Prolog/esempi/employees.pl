f1817576b4956777826904bb13119267108d8707
bigboss98 <marco_natali@ymail.com>
2018-11-20 21:50:15 +0100

    Inserite modifiche

---
+++ Linguaggi di Programmazione/Prolog/esempi/employees.pl
@@ -0,0 +1,18 @@
+%%%% -* mode:Prolog -*-
+
+colleague(X, Y):-
+    worksFor(X, Z),
+    worksFor(Y, Z),
+    X \= Y. %Utilizzato per dire che X != Y
+
+worksFor(ugo, ibm).
+worksFor(gino, ibm).
+worksFor(enrica, samsung).
+worksFor(salvo, olivetti).
+worksFor(ciro, samsung).
+
+worksForAnEvilCompany(X, Y) :- worksFor(X, trenord).
+
+:- colleague(X, Y).
+%%%% End of file employees.pl
+    
